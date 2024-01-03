0711bed937f1dd76213cabdd02eb45269eee1f16
RoseAlice2018 <1505243015@qq.com>
2024-01-03 22:24:56 +0800

    [Algorithm] add 0103/

--- 202401/0103.cpp
+++ 202401/0103.cpp
@@ -3,3 +3,46 @@
 #include<queue>
 #include<string>
 using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x) , next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if(head == nullptr)
+            return nullptr;
+
+        ListNode *child = removeNodes(head->next);
+        if(child == nullptr)
+            return head;
+        else 
+        {
+            if(child->val > head->val)
+            {
+                return child;
+            }
+            else 
+            {
+                head->next = child;
+                return head;
+            }
+        }
+        return nullptr;
+    }
+
+};
