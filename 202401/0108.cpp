a50feb02d30aef5fd6e8f22e7eb6339b3f69f8bc
RoseAlice2018 <1505243015@qq.com>
2024-01-08 23:56:38 +0800

    [Algorithm] add 0108/

--- 202401/0108.cpp
+++ 202401/0108.cpp
@@ -4,3 +4,25 @@
 #include<string>
 #include<unordered_map>
 using namespace std;
+class Solution {
+public:
+    int numberOfBoomerangs(vector<vector<int>> &points) {
+        int ans = 0;
+        for (auto &p : points) {
+            unordered_map<int, int> cnt;
+            for (auto &q : points) {
+                int dis = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]);
+                ++cnt[dis];
+            }
+            for (auto &[_, m] : cnt) {
+                ans += m * (m - 1);
+            }
+        }
+        return ans;
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/number-of-boomerangs/solutions/994189/hui-xuan-biao-de-shu-liang-by-leetcode-s-lft5/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
