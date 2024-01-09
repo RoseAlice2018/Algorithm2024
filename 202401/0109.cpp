a31cefcf12169ae70feb53c2c5dd26a168739d0c
RoseAlice2018 <1505243015@qq.com>
2024-01-09 23:28:32 +0800

    [Algorithm] add 0109/

--- 202401/0109.cpp
+++ 202401/0109.cpp
@@ -4,3 +4,29 @@
 #include<string>
 #include<unordered_map>
 using namespace std;
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.size();
+        vector<int> d(n + 1, INT_MAX);
+        unordered_map<string, int> mp;
+        for (auto s : dictionary) {
+            mp[s]++;
+        }
+        d[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            d[i] = d[i - 1] + 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (mp.count(s.substr(j, i - j))) {
+                    d[i] = min(d[i], d[j]);
+                }
+            }
+        }
+        return d[n];
+    }
+};
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/extra-characters-in-a-string/solutions/2590667/zi-fu-chuan-zhong-de-e-wai-zi-fu-by-leet-f0lu/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
