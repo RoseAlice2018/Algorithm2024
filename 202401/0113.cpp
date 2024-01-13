77d922c38486305eac4ec7633bd712ac2e52ca8d
RoseAlice2018 <1505243015@qq.com>
2024-01-13 20:42:36 +0800

    [Algorithm] add 0113/

--- 202401/0113.cpp
+++ 202401/0113.cpp
@@ -4,3 +4,37 @@
 #include<string>
 #include<unordered_map>
 using namespace std;
+const int N = 26;
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        vector<int> count(N);
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+        string ret;
+        int m = 0;
+        for (int i = N - 1, j = N - 2; i >= 0 && j >= 0;) {
+            if (count[i] == 0) { // 当前字符已经填完，填入后面的字符，重置 m
+                m = 0;
+                i--;
+            } else if (m < repeatLimit) { // 当前字符未超过限制
+                count[i]--;
+                ret.push_back('a' + i);
+                m++;
+            } else if (j >= i || count[j] == 0) { // 当前字符已经超过限制，查找可填入的其他字符
+                j--;
+            } else { // 当前字符已经超过限制，填入其他字符，并且重置 m
+                count[j]--;
+                ret.push_back('a' + j);
+                m = 0;
+            }
+        }
+        return ret;
+    }
+};
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/construct-string-with-repeat-limit/solutions/1300982/gou-zao-xian-zhi-zhong-fu-de-zi-fu-chuan-v02s/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
