c25cdab7c8c4a31a80d780d6757e713fba0ff976
RoseAlice2018 <1505243015@qq.com>
2024-01-17 23:57:15 +0800

    [Algorithm] add 0117/

--- 202401/0117.cpp
+++ 202401/0117.cpp
@@ -11,3 +11,19 @@ struct ListNode{
     ListNode(int x):val(x), next(nullptr) {}
     ListNode(int x, ListNode *next):val(x), next(next){}
 };
+class Solution {
+public:
+    int maximumNumberOfStringPairs(vector<string>& words) {
+        int n = words.size();
+        int ans = 0;
+        unordered_set<int> seen;
+        for (int i = 0; i < n; ++i) {
+            if (seen.count(words[i][1] * 100 + words[i][0])) {
+                ++ans;
+            }
+            seen.insert(words[i][0] * 100 + words[i][1]);
+        }
+        return ans;
+    }
+};
+
