58fdeed447e7b4d9a6c7bbff2785fe1ea11c8f62
RoseAlice2018 <1505243015@qq.com>
2024-01-23 20:59:30 +0800

    [Algorithm] add 0123/

--- 202401/0123.cpp
+++ 202401/0123.cpp
@@ -11,3 +11,27 @@ struct ListNode{
     ListNode(int x):val(x), next(nullptr) {}
     ListNode(int x, ListNode *next):val(x), next(next){}
 };
+
+class Solution {
+public:
+    int alternatingSubarray(vector<int>& nums) {
+        int res = -1;
+        int n = nums.size();
+        int firstIndex = 0;
+        for (int i = 1; i < n; i++) {
+            int length = i - firstIndex + 1;
+            if (nums[i] - nums[firstIndex] == (length - 1) % 2) {
+                res = max(res, length);
+            } else {
+                if (nums[i] - nums[i - 1] == 1) {
+                    firstIndex = i - 1;
+                    res = max(res, 2);
+                } else {
+                    firstIndex = i;
+                }
+            }
+        }
+        return res;
+    }
+};
+
