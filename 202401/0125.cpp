47869f7b78d7161af8e7ad92c05632efcfa0697e
RoseAlice2018 <1505243015@qq.com>
2024-01-25 19:50:35 +0800

    [Algorithm] add 0125/

--- 202401/0125.cpp
+++ 202401/0125.cpp
@@ -11,3 +11,29 @@ struct ListNode{
     ListNode(int x):val(x), next(nullptr) {}
     ListNode(int x, ListNode *next):val(x), next(next){}
 };
+class Solution {
+public:
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int ret = 0;
+        auto  countnum = [](int num, int k) ->int {
+            int count = 0;
+            while(num > 0)
+            {
+                if(num & 1 == 1)
+                    count++;
+                num >>= 1;
+            }
+            return k == count;
+        };
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(countnum(i, k))
+            {
+                ret += nums[i];
+            }
+        }
+        
+        return ret;
+    }
+};
