e63f1a00dc7c6b0da7c02ad571245554c7cc40bd
RoseAlice2018 <1505243015@qq.com>
2024-01-31 21:46:00 +0800

    add new headfile

--- create.sh
+++ create.sh
@@ -6,19 +6,20 @@ echo "${current_date}"
 current_year=$(date +"%Y%m" | awk '{print substr($0,1,6)}')
 echo "${current_year}!"
 
-#创建文件夹
+#创建文件�?
 mkdir -p "${current_year}"
 
-#创建cpp文件名
+#创建cpp文件�?
 cpp_file="${current_date}.cpp"
 
-#在cpp文件中添加内容
+#在cpp文件中添加内�?
 
 echo "#include<vector>
 #include<stack>
 #include<queue>
 #include<string>
 #include<unordered_map>
+#include<unordered_set>
 using namespace std;
 struct ListNode{
     int val;
@@ -28,7 +29,7 @@ struct ListNode{
     ListNode(int x, ListNode *next):val(x), next(next){}
 };" > "${cpp_file}"
 
-#移动文件到文件夹里
+#移动文件到文件夹�?
 if [ -e "${current_year}/${cpp_file}" ]; then  
     echo "File ${cpp_file} already exists in the target folder. Skipping move."
 else
@@ -37,5 +38,5 @@ else
     echo "Moved ${cpp_file} to ${current_year} successfully!"
 fi
 
-#输出创建成功的消息
+#输出创建成功的消�?
 echo "Created ${cpp_file} successfully!"
