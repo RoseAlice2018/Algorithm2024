528be139bf391d3a2a72f681b77af8695f390828
RoseAlice2018 <1505243015@qq.com>
2024-01-31 21:45:00 +0800

    [Algorithm] add 0131/

--- 202401/0131.cpp
+++ 202401/0131.cpp
@@ -3,6 +3,7 @@
 #include<queue>
 #include<string>
 #include<unordered_map>
+#include<unordered_set>
 using namespace std;
 struct ListNode{
     int val;
@@ -11,3 +12,26 @@ struct ListNode{
     ListNode(int x):val(x), next(nullptr) {}
     ListNode(int x, ListNode *next):val(x), next(next){}
 };
+
+class Solution {
+public:
+    vector<int> distinctDifferenceArray(vector<int>& nums) {
+        unordered_set<int> sufix;
+        unordered_set<int> prefix;
+        vector<int> sufix_count(nums.size() + 1);
+        sufix_count[nums.size()] = 0;
+        for(int i = nums.size() - 1; i >=0 ; --i)
+        {
+            sufix.insert(nums[i]);
+            sufix_count[i] = sufix.size();
+        }
+        vector<int> res(nums.size());
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            prefix.insert(nums[i]);
+            res[i] = prefix.size() - sufix_count[i + 1];
+        }
+
+        return res;
+    }
+}; 
