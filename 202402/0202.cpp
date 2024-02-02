d432df81f5a862221adbed6733a70eff795009b9
RoseAlice2018 <1505243015@qq.com>
2024-02-02 16:43:19 +0800

    [Algorithm] add 0202/

--- 202402/0202.cpp
+++ 202402/0202.cpp
@@ -12,3 +12,50 @@ struct ListNode{
     ListNode(int x):val(x), next(nullptr) {}
     ListNode(int x, ListNode *next):val(x), next(next){}
 };
+class Solution {
+public:
+    int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
+        unordered_set<int> chosen;
+        auto getmin = [](vector<int>& values, unordered_set<int>& _chosen)->int{
+            int idx = -1;
+            int max = 0;
+            for(int i = 0; i < values.size();i++)
+            {
+                if(_chosen.count(i))//skip
+                    continue;
+                if(values[i] >= max)
+                {
+                    max = values[i];
+                    idx = i;
+                }
+            }
+            return idx;
+        };
+
+        int A = 0, B = 0;
+        for(int i = 0; i < aliceValues.size(); i++)
+        {
+            int chose = getmin(aliceValues, chosen);
+            if(chose == -1) //done
+                break;
+            else 
+            {
+                chosen.insert(chose);
+                A += aliceValues[chose];
+            }
+            chose = getmin(bobValues, chosen);
+            if(chose == -1)
+                break;
+            else 
+            {
+                chosen.insert(chose);
+                B += bobValues[chose];
+            }
+        }
+        if(A > B)
+            return 1;
+        else if(A < B)
+            return -1;
+        return 0;
+    }
+};
