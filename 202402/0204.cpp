dd579cb3e4bf5a671aa97c5be349ec3a0da5abad
RoseAlice2018 <1505243015@qq.com>
2024-02-04 16:51:52 +0800

    [Algorithm] add 0204/

---
+++ 202402/0204.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4 != 0;      
+    }
+};
