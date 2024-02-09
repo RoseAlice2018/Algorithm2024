b602356e7ac85ce64b35f4579177e29b3d8462f1
RoseAlice2018 <1505243015@qq.com>
2024-02-09 14:54:47 +0800

    add 环形链表

---
+++ 分类/链表/环形链表.cpp
@@ -0,0 +1,42 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while(fast != nullptr)
+        {
+            if(fast->next == nullptr)
+                return false;
+            fast = fast->next->next;
+            if(fast == slow)
+                return true;
+            slow = slow->next;
+        }
+
+        return false;
+    }
+};
