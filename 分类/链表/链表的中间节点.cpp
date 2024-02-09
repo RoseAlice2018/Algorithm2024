d9e29e1a303f44d985ea7152fb99511ce9c3573b
RoseAlice2018 <1505243015@qq.com>
2024-02-09 14:30:35 +0800

    链表的中间节点

---
+++ 分类/链表/链表的中间节点.cpp
@@ -0,0 +1,47 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while(fast != nullptr)
+        {
+           if(fast->next == nullptr)
+           {
+                fast = fast->next;
+                //slow = slow->next;
+           } 
+           else 
+           {
+                fast = fast->next->next;
+                slow = slow->next;
+           }
+        }
+        return slow;
+    }
+};
