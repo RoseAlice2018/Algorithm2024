ca4bb53748760f6a9b56cfd6465d308ec70351c4
RoseAlice2018 <1505243015@qq.com>
2024-02-09 15:41:17 +0800

    add 相交链表

--- 分类/链表/相交链表.cpp
+++ 分类/链表/相交链表.cpp
@@ -12,3 +12,40 @@ struct ListNode{
     ListNode(int x):val(x), next(nullptr) {}
     ListNode(int x, ListNode *next):val(x), next(next){}
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if((headA == nullptr)||(headB == nullptr))
+            return nullptr;
+        ListNode *beginA = headA;
+        ListNode *beginB = headB;
+
+        while(beginA != beginB)
+        {
+            if(beginA == nullptr )
+            {
+                beginA = headB;
+            }
+            else 
+                beginA = beginA->next;
+
+            if(beginB == nullptr )
+            {
+                beginB = headA;
+            }
+            else 
+                beginB = beginB->next;
+        }
+
+        return beginA;
+    }
+};
