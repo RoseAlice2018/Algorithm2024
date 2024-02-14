3c409e903b4b8e71b07905dae46ae344e93b00b7
RoseAlice2018 <1505243015@qq.com>
2024-02-14 23:15:48 +0800

    [Algorithm] add 0214/

--- 202402/0214.cpp
+++ 202402/0214.cpp
@@ -12,3 +12,54 @@ struct ListNode{
     ListNode(int x):val(x), next(nullptr) {}
     ListNode(int x, ListNode *next):val(x), next(next){}
 };
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> level;
+
+        vector<vector<int>> ret;
+        if(root == nullptr)
+            return ret;
+
+        level.push(root);   
+        while(!level.empty())
+        {
+            int si = level.size();
+            
+            vector<int> l1;
+            for(int i = 0; i < si; ++i)
+            {
+                TreeNode *tmp = level.front();
+                level.pop();
+                l1.push_back(tmp->val);
+
+                if(tmp->left) level.push(tmp->left);
+                if(tmp->right) level.push(tmp->right);
+            }
+            ret.push_back(l1);
+        }
+        
+        return ret;
+    }
+};
