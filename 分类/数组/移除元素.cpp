877487b92cb13059bfe0650b2901e97f436633ec
RoseAlice2018 <1505243015@qq.com>
2024-02-17 19:00:14 +0800

    add 分类/数组/移除元素.cpp

--- 分类/数组/移除元素.cpp
+++ 分类/数组/移除元素.cpp
@@ -12,3 +12,22 @@ struct ListNode{
     ListNode(int x):val(x), next(nullptr) {}
     ListNode(int x, ListNode *next):val(x), next(next){}
 };
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0;
+        int fast = 0;
+        while(fast < nums.size())
+        {
+            if(nums[fast] != val)
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+
+        return slow ;
+    }
+};
