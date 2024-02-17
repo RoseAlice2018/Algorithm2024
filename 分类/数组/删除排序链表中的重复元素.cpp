4f25fd795bb4d87b9bc12185bb6db2c16b29b600
RoseAlice2018 <1505243015@qq.com>
2024-02-17 18:52:57 +0800

    add 分类/数组/删除排序链表中的重复元素.cpp

---
+++ 分类/数组/删除排序链表中的重复元素.cpp
@@ -0,0 +1,47 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        if(fast == nullptr)
+            return head;
+
+        while(fast != nullptr)
+        {
+            if(fast->val != slow->val)
+            {
+                slow->next = fast;
+                slow = fast;
+            }
+            fast = fast->next;
+        }
+
+        slow->next = nullptr;
+        return head;
+    }
+};
