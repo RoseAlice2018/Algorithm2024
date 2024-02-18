c5498e5dba2834ed88feea556f3fd2437292e08b
RoseAlice2018 <1505243015@qq.com>
2024-02-18 22:03:58 +0800

    二叉树的直径

---
+++ 分类/二叉树/二叉树的直径.cpp
@@ -0,0 +1,57 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res;
+    int depth(TreeNode *root)
+    {
+        if(root == nullptr)
+            return 0;
+        
+        int L = depth(root->left);
+        int R = depth(root->right);
+        int tmp = L + R + 1;
+        res = max(res, tmp);
+
+        return max(L, R) + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        res = 0;
+        depth(root);
+        return res - 1;
+
+    }
+};
