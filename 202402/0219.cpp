12b564bdf9b27e6b77cf55b25a4e7a18d016b722
RoseAlice2018 <1505243015@qq.com>
2024-02-19 22:02:51 +0800

    [Algorithm] add 0219/

---
+++ 202402/0219.cpp
@@ -0,0 +1,43 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
+};
+
+class Solution {
+public:
+    void helper(const Node* root, vector<int> & res) {
+        if (root == nullptr) {
+            return;
+        }
+        for (auto & ch : root->children) {
+            helper(ch, res);
+        }
+        res.emplace_back(root->val);
+    }
+
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        helper(root, res);
+        return res;
+    }
+};
+
