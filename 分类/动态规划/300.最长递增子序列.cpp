44155acb57d013bfe15c83a202d3bcda08145f46
RoseAlice2018 <1505243015@qq.com>
2024-02-25 00:13:19 +0800

    add cclass Solution { public:     int lengthOfLIS(vector<int>& nums) {         int n = (int)nums.size();         if (n == 0) {             return 0;         }         vector<int> dp(n, 0);         for (int i = 0; i < n; ++i) {             dp[i] = 1;             for (int j = 0; j < i; ++j) {                 if (nums[j] < nums[i]) {                     dp[i] = max(dp[i], dp[j] + 1);                 }             }         }         return *max_element(dp.begin(), dp.end());     } };
    作者：力扣官方题解
    链接：https://leetcode.cn/problems/longest-increasing-subsequence/solutions/147667/zui-chang-shang-sheng-zi-xu-lie-by-leetcode-soluti/
    来源：力扣（LeetCode）
    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。

---
+++ 分类/动态规划/300.最长递增子序列.cpp
@@ -0,0 +1,43 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
+};
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = (int)nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> dp(n, 0);
+        for (int i = 0; i < n; ++i) {
+            dp[i] = 1;
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+
