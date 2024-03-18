0c787d6fa3cde25a63534d47fafe8e8b030b256d
RoseAlice2018 <1505243015@qq.com>
2024-03-18 23:45:34 +0800

    [Algorithm] add 0318/

---
+++ 202403/0318.cpp
@@ -0,0 +1,44 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
+};
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+        _data = nums;
+    }
+    
+    int sumRange(int left, int right) {
+        int ret = 0;
+        for(int i = left; i<=right; i++)
+            ret+=_data[i];
+        return ret;
+    }
+    vector<int> _data;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
