e6811fd7475579b2f3ae20c9cb7479948b89bf14
RoseAlice2018 <1505243015@qq.com>
2024-03-20 23:58:51 +0800

    [Algorithm] add 0320/

--- 202403/0320.cpp
+++ 202403/0320.cpp
@@ -21,3 +21,32 @@ struct TreeNode{
     TreeNode(int x):val(x), left(nullptr), right(nullptr){}
     TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
 };
+
+class Solution {
+public:
+    long long fastPow(long long x, long long n, long long mod) {
+        long long res = 1;
+        for (; n != 0; n >>= 1) {
+            if (n & 1) {
+                res = res * x % mod;
+            }
+            x = x * x % mod;
+        }
+        return res;
+    }
+    
+    int minNonZeroProduct(int p) {
+        if (p == 1) {
+            return 1;
+        }
+        long long mod = 1e9 + 7;
+        long long x = fastPow(2, p, mod) - 1;
+        long long y = (long long) 1 << (p - 1);
+        return fastPow(x - 1, y - 1, mod) * x % mod;
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/minimum-non-zero-product-of-the-array-elements/solutions/2688955/shu-zu-yuan-su-de-zui-xiao-fei-ling-chen-xti0/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
