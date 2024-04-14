37131299c404579e1f285be6aa40a46a021fa072
RoseAlice2018 <1505243015@qq.com>
2024-04-14 16:56:44 +0800

    [Algorithm] add 0414/

---
+++ 202404/0414.cpp
@@ -0,0 +1,105 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
+};
+
+class MyHashSet {
+public:
+    MyHashSet() {
+
+    }
+    
+    void add(int key) {
+        set[key] = true;
+    }
+    
+    void remove(int key) {
+        set[key] = false;
+    }
+    
+    bool contains(int key) {
+        return set[key];
+    }
+
+    bool set[100001];
+};
+
+// 链地址法
+
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
+
+class MyHashSet {
+private:
+    vector<list<int>> data;
+    static const int base = 769;
+    static int hash(int key) {
+        return key % base;
+    }
+public:
+    /** Initialize your data structure here. */
+    MyHashSet(): data(base) {}
+    
+    void add(int key) {
+        int h = hash(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it) == key) {
+                return;
+            }
+        }
+        data[h].push_back(key);
+    }
+    
+    void remove(int key) {
+        int h = hash(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it) == key) {
+                data[h].erase(it);
+                return;
+            }
+        }
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        int h = hash(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it) == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/design-hashset/solutions/652778/she-ji-ha-xi-ji-he-by-leetcode-solution-xp4t/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
