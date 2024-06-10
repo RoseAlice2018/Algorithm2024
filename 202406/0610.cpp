657eb602f3bb89787b74aef93f87fce63252b266
RoseAlice2018 <1505243015@qq.com>
2024-06-10 22:43:34 +0800

    [Algorithm] add 0610/

---
+++ 202406/0610.cpp
@@ -0,0 +1,51 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
+};
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int ans = 0;
+        int left = 0, right = people.size() - 1;
+        while(left < right)
+        {
+            if(people[left] + people[right] > limit)
+            {
+                right--;
+                ans++;
+                continue;
+            }
+            else
+            {
+                left++;
+                right--;
+                ans++;
+                continue;
+            }
+        }
+        if(left == right)
+            ans++;
+        return ans;
+    }
+};
