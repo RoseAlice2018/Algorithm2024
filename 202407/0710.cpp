2937cf9835ea0b19df6b11b31e0aa279f49b4ccb
RoseAlice2018 <1505243015@qq.com>
2024-07-10 23:54:57 +0800

    [Algorithm] add 0710/

---
+++ 202407/0710.cpp
@@ -0,0 +1,45 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
+};
+
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        int l = 1;
+        while(l < n && nums[l - 1] < nums[l])
+            l++;
+        res += l + (l < n);
+        for (int r = n - 2; r >= 0; r--) {
+            while (l > 0 && nums[l - 1] >= nums[r + 1]) {
+                l--;
+            }
+            res += l + (l <= r);
+            if (nums[r] >= nums[r + 1]) {
+                break;
+            }
+        }  
+        return res;
+    }
+};
