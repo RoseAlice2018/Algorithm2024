eccdfd7aa9d7a7e3206faf27ff646bf1f88c96c4
RoseAlice2018 <1505243015@qq.com>
2025-07-19 18:54:30 +0800

    [Algorithm] add 0719/

--- 202507/0719.cpp
+++ 202507/0719.cpp
@@ -110,3 +110,24 @@ public:
         return s.substr(ans_l, max_len);
     }
 };
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int, int> index;
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            if(index.find(target - nums[i]) != index.end())
+            {
+                ans.push_back(index[target - nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            index[nums[i]] = i;
+        }
+        return ans;
+    }
+};
