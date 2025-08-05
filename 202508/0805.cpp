d2c7c3540e8a818c7a2e3c00bc279b2da37e1f90
RoseAlice2018 <1505243015@qq.com>
2025-08-05 15:41:53 +0800

    [Algorithm] add 0805/

--- 202508/0805.cpp
+++ 202508/0805.cpp
@@ -71,3 +71,93 @@ public:
         return -1;
     }
 };
+class Solution {
+public:
+    int getvalue(string v)
+    {
+        int ret = 0;
+        for(int i = 0; i < v.size();i++)
+        {
+            ret = ret * 10 + v[i] - '0';
+        }
+        return ret;
+    }
+    int _compare(string version1, string version2)
+    {
+        int v1 = getvalue(version1);
+        int v2 = getvalue(version2);
+        cout<<v1<<"XXX"<<v2<<endl;
+        if(v1 == v2) return 0;
+        return v1 > v2 ? 1 : -1;
+    }
+
+    vector<string> split(string version)
+    {
+        string tmp;
+        vector<string> ret;
+        for(int i = 0; i < version.size(); i++)
+        {
+            if(version[i] == '.')
+            {
+                ret.emplace_back(tmp);
+                tmp.clear();
+                for(int j = i + 1; j < version.size(); j++)
+                {
+                    if(version[j] == '0')
+                        continue;
+                    else{
+                        i = j - 1;
+                    }
+                }
+            }
+            else{
+                tmp.push_back(version[i]);
+            }
+        }
+        ret.push_back(tmp);
+        return ret;
+    }
+
+    int compareVersion(string version1, string version2) {
+        vector<string> v1 = split(version1);
+        vector<string> v2 = split(version2);
+        for(int i = 0; i < max(v1.size(), v2.size()); i++)
+        {
+            string tmp1 =  i < v1.size() ? v1[i] : "0";
+            cout<<tmp1<<" "<<v1.size()<<endl;;
+            string tmp2 =  i < v2.size() ? v2[i] : "0";
+            cout<<tmp2<<" "<<v2.size()<<endl;;
+            int ret = _compare(tmp1, tmp2);
+            cout<<ret<<endl;
+            if(ret == 0)
+                continue;
+            return ret;
+        }
+        return 0;
+    }
+};
+
+class Solution {
+public:
+	int compareVersion(string version1, string version2) {
+		stringstream ss;
+		vector<int> v1, v2;
+		string s;
+		ss << version1;
+		while (getline(ss, s, '.')) {
+			v1.push_back(stoi(s));
+		}
+		ss.clear();
+		ss << version2;
+		while (getline(ss, s, '.')) {
+			v2.push_back(stoi(s));
+		}
+		while (v1.size() < v2.size())	v1.push_back(0);
+		while (v2.size() < v1.size())	v2.push_back(0);
+		for (int i = 0; i < v1.size(); ++i) {
+			if (v1[i] > v2[i])	return 1;
+			else if (v1[i] < v2[i])	return -1;
+		}
+		return 0;
+	}
+};
