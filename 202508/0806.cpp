af89475f5abb43ad8ce3c956c216dd5dbf72da82
RoseAlice2018 <1505243015@qq.com>
2025-08-06 16:01:22 +0800

    [Algorithm] add 0806/

---
+++ 202508/0806.cpp
@@ -0,0 +1,98 @@
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x):val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next):val(x), next(next){}
+};
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
+};
+
+
+class Solution {
+public:
+    void backtrace(int n, int left, int right, vector<string> &ret, string &path)
+    {
+        if(n == 0 && (left == right))
+        {
+            ret.push_back(path);
+            return;
+        }
+        if(n <= 0)
+            return;
+        if(left < right)
+        {
+            return;
+        }
+        // 1 chose left
+        path.push_back('(');
+        backtrace(n-1, left+1, right, ret, path);
+        path.pop_back();
+
+        // 2 chose right
+        path.push_back(')');
+        backtrace(n-1, left, right + 1, ret, path);
+        path.pop_back();
+        return;
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ret;
+        string path;
+        backtrace(2*n, 0, 0, ret, path);
+        return ret;
+    }
+};
+class Solution {
+public:
+    void backtrace(int n, int left, int right, vector<string> &ret, string &path, int limit)
+    {
+        if(n == 0 && (left == right))
+        {
+            ret.push_back(path);
+            return;
+        }
+        if(n <= 0)
+            return;
+        if(left < right)
+        {
+            return;
+        }
+        if(left < limit)
+        {
+            // 1 chose left
+            path.push_back('(');
+            backtrace(n-1, left+1, right, ret, path, limit);
+            path.pop_back();
+        }
+        if(right < limit)
+        {
+            // 2 chose right
+            path.push_back(')');
+            backtrace(n-1, left, right + 1, ret, path, limit);
+            path.pop_back();
+            return;
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ret;
+        string path;
+        backtrace(2*n, 0, 0, ret, path, n);
+        return ret;
+    }
+};
