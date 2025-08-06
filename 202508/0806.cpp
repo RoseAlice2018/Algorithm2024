092c458ab8ebe1df5b9f62c331e78db0b71dd48d
RoseAlice2018 <1505243015@qq.com>
2025-08-06 22:37:40 +0800

    [Algorithm] add 0806/

--- 202508/0806.cpp
+++ 202508/0806.cpp
@@ -190,3 +190,22 @@ public:
         return dummy->next;
     }
 };
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int first = 1; 
+        int second = 2;
+        int third;
+        for(int i = 3; i<= n; i++)
+        {
+            third= first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+};
