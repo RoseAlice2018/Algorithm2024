4fe5e985c8ad42a82759fdf45b39a45ded8caedb
RoseAlice2018 <1505243015@qq.com>
2025-08-07 15:50:03 +0800

    [Algorithm] add 0807/

--- 202508/0807.cpp
+++ 202508/0807.cpp
@@ -50,3 +50,24 @@ public:
         return slow;
     }
 };
+
+class Solution {
+public:
+    unordered_map<int, int> dict;
+    TreeNode * _buildTree(vector<int> &preorder, vector<int> &inorder, int pre_index, int left, int right)
+    {
+        if(left > right) return nullptr;
+        TreeNode * root = new TreeNode(preorder[pre_index]);
+        int inorder_index = dict[preorder[pre_index]];
+        int left_size = inorder_index - left;
+        if(left_size)
+            root->left = _buildTree(preorder, inorder, pre_index + 1, left, inorder_index - 1);
+        root->right = _buildTree(preorder, inorder, pre_index + left_size + 1, inorder_index + 1, right);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for(int i = 0; i < inorder.size();i++)
+            dict[inorder[i]] = i;
+        return _buildTree(preorder, inorder, 0, 0, inorder.size() - 1);
+    }
+};
