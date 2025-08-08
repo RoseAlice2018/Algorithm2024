b219bf57505ff5dc8fc9482b57ac6308c5fa7732
RoseAlice2018 <1505243015@qq.com>
2025-08-08 18:04:47 +0800

    [Algorithm] add 0808/

--- 202508/0808.cpp
+++ 202508/0808.cpp
@@ -104,3 +104,30 @@ public:
         return ret;
     }
 };
+
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return height(root) >= 0;
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/balanced-binary-tree/solutions/377216/ping-heng-er-cha-shu-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
