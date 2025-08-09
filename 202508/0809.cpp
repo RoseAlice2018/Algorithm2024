58226a0d0ae8256703df5904bdfd23bc19269b18
RoseAlice2018 <1505243015@qq.com>
2025-08-09 18:15:26 +0800

    [Algorithm] add 0809/

--- 202508/0809.cpp
+++ 202508/0809.cpp
@@ -79,3 +79,24 @@ public:
 链接：https://leetcode.cn/problems/longest-consecutive-sequence/solutions/276931/zui-chang-lian-xu-xu-lie-by-leetcode-solution/
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // 
+        int n = matrix.size();
+        for(int i = 0; i < n/2; i++)
+        {
+            for(int j = 0; j < n; j++)
+                swap(matrix[i][j], matrix[n - i - 1][j]);
+        }
+
+        // 
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j <= i; j++)
+                swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+};
