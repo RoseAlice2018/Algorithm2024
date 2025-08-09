a181f288ae52ed8fc2d38ae0d50be8660b2eb880
RoseAlice2018 <1505243015@qq.com>
2025-08-09 18:43:55 +0800

    [Algorithm] add 0809/

--- 202508/0809.cpp
+++ 202508/0809.cpp
@@ -147,3 +147,23 @@ public:
         return max(dp[n - 1][0], dp[n-1][2]);
     }
 };
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int dp[n][2];
+        dp[0][0] = 0, dp[0][1] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/solutions/476791/mai-mai-gu-piao-de-zui-jia-shi-ji-ii-by-leetcode-s/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
