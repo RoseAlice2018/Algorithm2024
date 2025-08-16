4a89521fcd48c70356b812587b16bdc1e3a92c4c
RoseAlice2018 <1505243015@qq.com>
2025-08-16 11:42:39 +0800

    [Algorithm] add 0816/

--- 202508/0816.cpp
+++ 202508/0816.cpp
@@ -39,3 +39,48 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        vector <long> maxF(nums.begin(),nums.end()), minF(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); ++i) {
+            maxF[i] = max(maxF[i - 1] * nums[i], max((long)nums[i], minF[i - 1] * nums[i]));
+            minF[i] = min(minF[i - 1] * nums[i], min((long)nums[i], maxF[i - 1] * nums[i]));
+            if(minF[i]<INT_MIN) {
+                minF[i]=nums[i];
+            }
+        }
+        return *max_element(maxF.begin(), maxF.end());
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/maximum-product-subarray/solutions/250015/cheng-ji-zui-da-zi-shu-zu-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        int size = nums.size();
+        if (size == 1) {
+            return nums[0];
+        }
+        vector<int> dp = vector<int>(size, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < size; i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[size - 1];
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/house-robber/solutions/263856/da-jia-jie-she-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
