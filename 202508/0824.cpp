8b7e9a603e582a58e1913a7bb8a3b43dd542e858
RoseAlice2018 <1505243015@qq.com>
2025-08-24 17:32:55 +0800

    [Algorithm] add 0824/

--- 202508/0824.cpp
+++ 202508/0824.cpp
@@ -61,3 +61,37 @@ public:
         return deepsearch(root, targetSum);
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode * cur = dummy->next;
+        ListNode * pre = dummy;
+        while(cur)
+        {
+            if(cur->next != nullptr)
+            {
+                while(cur->next && cur->val == cur->next->val)
+                {
+                    cur = cur->next;
+                }
+            }
+            pre->next = cur;
+            pre = cur;
+            cur = cur->next;
+        }
+        return dummy->next;
+    }
+};
