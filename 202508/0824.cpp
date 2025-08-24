17157e89655e799bb82853e7eabccce930385483
RoseAlice2018 <1505243015@qq.com>
2025-08-24 21:08:15 +0800

    [Algorithm] add 0824/

--- 202508/0824.cpp
+++ 202508/0824.cpp
@@ -203,3 +203,31 @@ public:
         return nums;
     }
 };
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        auto wordDictSet = unordered_set <string> ();
+        for (auto word: wordDict) {
+            wordDictSet.insert(word);
+        }
+
+        auto dp = vector <bool> (s.size() + 1);
+        dp[0] = true;
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && wordDictSet.find(s.substr(j, i - j)) != wordDictSet.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.size()];
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/word-break/solutions/302471/dan-ci-chai-fen-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
