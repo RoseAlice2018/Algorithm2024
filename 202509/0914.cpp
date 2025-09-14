b07079c71fbb409e5ea539bce718b7cbda0ac296
RoseAlice2018 <1505243015@qq.com>
2025-09-14 20:00:21 +0800

    [Algorithm] add 0914/

--- 202509/0914.cpp
+++ 202509/0914.cpp
@@ -230,3 +230,36 @@ public:
         reverse(nums.begin(), nums.end());
     }
 };
+
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        int ret = 0;
+        for(int i = 1; i < s.size(); i++)
+        {
+            if(s[i] == ')')
+            {
+                if(s[i-1] == '(')
+                {
+                    dp[i] = (i - 2) >= 0? dp[i-2] + 2 : 2;
+                }
+                else if(s[i-1] == ')')
+                {
+                    int index = i - dp[i-1] - 1;
+                    if(index>=0)
+                    {
+                        if(s[index] == '(')
+                        {
+                            dp[i] = (index - 1) >= 0 ? dp[index-1] + dp[i-1] + 2 : dp[i-1] + 2 ;
+                        }
+                    }
+                }
+                ret = max(ret, dp[i]);
+            }
+        }
+        return ret;
+    }
+};
