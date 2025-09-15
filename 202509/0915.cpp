901c62cae5fe3711fd674913c38e56b1ac9737d9
RoseAlice2018 <1505243015@qq.com>
2025-09-15 18:29:07 +0800

    [Algorithm] add 0915/

--- 202509/0915.cpp
+++ 202509/0915.cpp
@@ -21,3 +21,36 @@ struct TreeNode{
     TreeNode(int x):val(x), left(nullptr), right(nullptr){}
     TreeNode(int x, TreeNode *left, TreeNode *right):val(x), left(left), right(right){}
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    int dfs(TreeNode *root)
+    {
+        if(root == nullptr) return 0;
+        if(root->left == nullptr && root->right == nullptr) return 0;
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        int ret = left + right;
+        if(root->left) ret++;
+        if(root->right) ret++;
+
+        if(ret > ans) ans = ret;
+        return max(left, right) + 1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
