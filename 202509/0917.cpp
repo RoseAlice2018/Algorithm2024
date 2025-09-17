2ebc0352e467351b2c1b0f9ac3d70530362a7fbd
RoseAlice2018 <1505243015@qq.com>
2025-09-17 18:54:30 +0800

    [Algorithm] add 0917/

--- 202509/0917.cpp
+++ 202509/0917.cpp
@@ -60,6 +60,101 @@ public:
 };
 
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+       if(root == nullptr) return 0;
+       queue<TreeNode*> layer;
+       layer.push(root);
+       int ret = 1;
+       while(!layer.empty())
+       {
+            int len = layer.size();
+            int begin_idx = -1, end_idx = -1;
+            for(int i = 0; i < len; i++)
+            {
+                TreeNode *tmp = layer.front();
+                layer.pop();
+                if(tmp == nullptr) {
+                    layer.push(nullptr);
+                    layer.push(nullptr);
+                    continue;
+                }
+                if(begin_idx == -1){
+                    begin_idx = i;
+                    end_idx = i;
+                }
+                else{
+                    end_idx = i;
+                }
+                layer.push(tmp->left);
+                layer.push(tmp->right);
+            }
+            ret = max(end_idx - begin_idx + 1, ret);
+            if(begin_idx == -1){
+                break;
+            }
+       } 
+       return ret;
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(root == nullptr) return 0;
+        int index = 1;
+        queue<pair<TreeNode*,unsigned long long>> layer;
+        layer.push({root, 1});
+
+        unsigned long long ret = 1;
+        while(!layer.empty())
+        {
+            int len = layer.size();
+            unsigned long long left_index = -1, right_index = -1;
+            for(int i = 0; i < len; i++)
+            {
+                auto tmp = layer.front();
+                layer.pop();
+                if(left_index == -1)
+                {
+                    left_index = tmp.second;
+                    right_index = tmp.second;
+                }
+                else{
+                    right_index = tmp.second;
+                }
+                if(tmp.first->left) layer.push({tmp.first->left, 2 * tmp.second});
+                if(tmp.first->right) layer.push({tmp.first->right, 2 * tmp.second + 1});
+            }
+            ret = max(ret, right_index - left_index + 1);
+        }
+        return ret;
+    }
+};
 
 
 
